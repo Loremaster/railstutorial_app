ffc2fd8585d689f26e8ec38ad1193ec0324ca3d7
L Serj <gelvior@gmail.com>
2012-01-13 00:12:44 +0400

    Added few tests of Searches controller (checking Response and name of title). Also main CSS file (custom.css) moved from vendor/assets/stylesheets to app/assets/stylesheets.

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,5 +1,78 @@
-#require 'spec_helper'
+#"NOTICE:  text-search query doesn't contain lexemes: "" " fixed.
 
-#describe SearchesController do
+require 'spec_helper'
 
-#end
+describe SearchesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  describe "GET 'finding_microposts'" do
+    describe "when not signed in" do
+      before(:each) do
+        get :find_microposts
+      end
+
+      it "should not be successful" do
+        response.should_not be_success
+      end
+
+      it "should not have 'searching microposts' title" do
+        response.should_not have_selector( "title",
+                                           :content => "#{@base_title} | Searching microposts")
+      end
+    end
+
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in( Factory(:user) )
+        #controller.stub!(:params).and_return({"q" =>"Minus"})
+        get :find_microposts, :q => "Data"                                    #Sending data (by :q => "Text"), because postgre doesn't like empty strings and show warning.
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have right title" do
+        response.should have_selector( "title",
+                                       :content => "#{@base_title} | Searching microposts")
+      end
+    end
+  end
+
+  describe "GET 'find_users'" do
+    describe "when not signed in" do
+      before(:each) do
+        get :find_users
+      end
+
+      it "should not be successful" do
+        response.should_not be_success
+      end
+
+      it "should not have 'searching microposts' title" do
+        response.should_not have_selector( "title",
+                                           :content => "#{@base_title} | Searching microposts")
+      end
+    end
+
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in( Factory(:user) )
+        get :find_users, :q => "Another data"                                 #Sending data (by :q => "Text"), because postgre doesn't like empty strings and show warning.
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have right title" do
+        response.should have_selector( "title",
+                                       :content => "#{@base_title} | Searching users")
+      end
+    end
+  end
+end
