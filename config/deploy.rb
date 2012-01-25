23ca70d03e5bae99bcd66287f6f38eda3250ad35
L Serj <gelvior@gmail.com>
2012-01-25 21:16:17 +0400

    Set RVM in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user  # Don't use system-wide RVM
+
 #require 'bundler/capistrano'
 
 set :user, "root"                                                             #If you log into your server with a different user name than you are logged into your local machine with, you’ll need to tell Capistrano about that user name.
