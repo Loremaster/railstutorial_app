a48ab808c2a15cb696a96eceef2d4e444630d427
L Serj <gelvior@gmail.com>
2012-01-25 23:20:23 +0400

    Add some settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 
 set :rvm_ruby_string, '1.9.3'
-set :rvm_type, :user  # Don't use system-wide RVM
+set :rvm_type, :root  # Don't use system-wide RVM
 
 require 'bundler/capistrano'
 
