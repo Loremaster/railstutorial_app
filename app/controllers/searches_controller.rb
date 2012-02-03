69faa4a97bac5961848b9ec69c91e57f0e2f28ee
L Serj <gelvior@gmail.com>
2012-02-03 18:26:36 +0400

    Fix bug with links to delete of user's posts in search results.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
 
   def find_microposts
     @title = "Searching microposts"
-    @searches = Micropost.search params[:q], :page => params[:page], :per_page => 10
+    @searches = Micropost.search params[:q], :page => params[:page], :per_page => 7
     @key_word = params[:q]
     @params_of_coding = params[:utf8]
   end
